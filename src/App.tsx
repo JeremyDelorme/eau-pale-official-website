fbf9b98fc45fd997fb9c2d9ea375004d666943d8
JeremyDelorme <jeremydelorme23.7@gmail.com>
2022-09-08 12:02:37 -0400

    Add homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // General Imports
 import React, { FC } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Pages Imports
 import HomePage from './pages/HomePage';
@@ -15,15 +15,15 @@ import { ShoppingCartProvider } from './context/ShoppingCartContext';
 const App: FC = () => {
   return (
     <ShoppingCartProvider>
-      <BrowserRouter>
-        <Routes>
+      <Router basename={process.env.PUBLIC_URL}>
+        <Routes >
           <Route path='/eau-pale-official-website' element={<HomePage />} />
           <Route path='/eau-pale-official-website/music' element={<MusicPage />} />
           <Route path='/eau-pale-official-website/shows' element={<ShowsPage />} />
           <Route path='/eau-pale-official-website/shop' element={<StorePage />} />
           <Route path='/eau-pale-official-website/contact' element={<ContactPage />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </ShoppingCartProvider>
   );
 };
